c23430bf8fdcc9230d1fe52a0698997beedaf798
tom13878 <tomas.morley@wur.nl>
2016-09-16 10:56:51 +0200

    Added a little code for grad descent

--- Assignment1.py
+++ Assignment1.py
@@ -14,10 +14,10 @@ from scipy import stats
 import statsmodels.formula.api as sm
 
 # SETUP
-M_path
-T_path
+# path = 'C:\\Users\\marc\\Dropbox\\private Dokumente\\Studium\\Master\\Courses\\Advances in Data Mining\\Assignment 1\\ml-1m'
+path = 'C:/Users/Tomas/OneDrive/Documents/leiden/AiDM/ml-1m'
 os.getcwd()
-os.chdir('C:\\Users\\marc\\Dropbox\\private Dokumente\\Studium\\Master\\Courses\\Advances in Data Mining\\Assignment 1\\ml-1m')
+os.chdir(path)
 
 # pass in column names for each CSV and read them using pandas. 
 # Column names available in the readme file
@@ -29,8 +29,7 @@ users = pd.read_csv('users.dat', sep='::', names=u_cols,
 
 #Reading ratings file:
 r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
-ratings = pd.read_csv('ratings.dat', sep='::', names=r_cols,
- encoding='latin-1')
+ratings = pd.read_csv('ratings.dat', sep='::', names=r_cols, encoding='latin-1')
 
 #Reading items file:
 i_cols = ['movie id', 'movie title' ,'genre']
@@ -92,21 +91,38 @@ J = 6040
 U = np.random.rand(I,K)
 M = np.random.rand(K,J)
 
+# test with smaller dataset
+X_red = X.loc[:, 1:500]
+I = 3706
+K = 2
+J = 500
 
+# make NAs zero
+where_are_NaNs = np.isnan(X_red)
+X_red[where_are_NaNs] = 0
 
-### wrong
-U_red = U[0:500]
-where_are_NaNs = np.isnan(U_red)
-U_red[where_are_NaNs] = 0
-M_red = np.transpose(U_red)
+# generate random starting values u and m
+U = np.random.rand(I,K)
+M = np.random.rand(K,J)
 
-UM = np.dot(U_red, M_red)
-U_red.shape
-M_red.shape
-UM.shape
-###
+# calcualte SE
+X_hat = np.dot(U, M)
+e = X_red - X_hat
+SE = (e**2).sum()
 
-test = np.dot(U, M)
-test.shape
+eta = 0.01
+lam = 0.01
+
+# work out derivative
+step_U = -2*np.dot(e, M.transpose())
+step_M = -2*np.dot(U.transpose(), e)
+
+# take a step towards direction
+# implied by derivative
+U = U + eta*step_U
+M = M + eta*step_M
+
+# repeat process until SE is low enough
+# and then keep those us and ms
 
 
